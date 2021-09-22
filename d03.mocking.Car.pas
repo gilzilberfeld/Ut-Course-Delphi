426f80d5a728d96d59ee65e7eafd3eaaee31004e
gilzilberfeld <gil.zilberfeld@gmail.com>
2021-09-22 15:48:27 +0300

    removed comments

--- d03.mocking.Car.pas
+++ d03.mocking.Car.pas
@@ -11,32 +11,6 @@ ICar = interface(IInterface)
     procedure Start();
 end;
 
-//
-//type
-//  TCar = class (TInterfacedObject, ICar)
-//  public
-//    Function IsRunning: Boolean;
-//    Procedure SetAC(ac: TAirCondition);
-//    procedure Start();
-//  end;
-//
-//implementation
-//
-//Function TCar.IsRunning: Boolean;
-//begin
-//  raise Exception.Create('Not Implemented');
-//end;
-//
-//procedure TCar.SetAC(ac: TAirCondition);
-//begin
-//  raise Exception.Create('Not Implemented');
-//end;
-//
-//procedure TCar.Start;
-//begin
-//  raise Exception.Create('Not Implemented');
-//end;
-//
 implementation
 
 end.
